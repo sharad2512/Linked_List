77a0ac50e21844719e8e341f272a957618878575
sharad2512 <shnarute95@gmail.com>
2023-04-08 10:03:47 +0530

    Welcome to linked list data structure programing

---
+++ src/com/linked_List/App.java
@@ -0,0 +1,7 @@
+package com.linked_List;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome message to linked list data structure program");
+    }
+}
