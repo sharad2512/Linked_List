4b23694caeef29b7a14014be793a25dd749fcb6d
sharad2512 <shnarute95@gmail.com>
2023-04-08 10:26:29 +0530

    ADDED : code to adding data at in between

--- src/com/linked_List/Main.java
+++ src/com/linked_List/Main.java
@@ -7,12 +7,17 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 1 to add the data at the start"); // UC 2
         System.out.println("Enter 2 to add the data at the End"); // UC 3
+        System.out.println("Enter 3 to add the data at In Between ");
         switch (scanner.nextInt()) {
+
             case 1:
                 Operation.addDataAtStart();
                 break;
             case 2:
                 Operation.addDataAtEnd();
+                break;
+            case 3:
+                Operation.insertInBetween();
                 break;
         }
         scanner.close();
